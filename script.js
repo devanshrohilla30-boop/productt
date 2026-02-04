9234fe52d8b59dcd4a526299740983622c83e3e8
devanshrohilla30-boop <devanshrohilla30@gmail.com>
2026-02-04 20:40:13 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// console.log("Hello, World!");
+// let heading = document.getElementById("heading");
+// console.log(heading);
+// let para=document.getElementsByClassName("paragraph");
+// console.log(para)
+// let tag= document.getElementsByTagName("p");
+// console.log(tag)
+
+
+
+let heading = document.querySelector("#heading")
+console.log(heading)
+
+let firstElement = document.querySelector(".paragraph")
+console.log(firstElement)
+let para = document.querySelectorAll(".paragraph")
+console.log(para)
+let tag = document.querySelectorAll("p")
+console.log(tag)
